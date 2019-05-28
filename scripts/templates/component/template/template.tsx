883239d79b5748828624eacd10b79e7a9dcdaf89
Tibor Pilz <tibor@pilz.berlin>
2019-05-28 13:05:01 +0200

    Added template and script for adding component

---
+++ scripts/templates/component/template/template.tsx
@@ -0,0 +1,12 @@
+import { State, Prop, Listen, Component, h } from '@stencil/core';
+
+@Component({
+  tag: 'vaw-template',
+  styleUrl: 'template.scss',
+  shadow: false
+})
+export class Template {
+  render() {
+    return `template`;
+  }
+}
